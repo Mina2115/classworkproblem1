06f36f0f751391e9caacced53ecf2b3d2b415a36
Mina2115 <78247742+Mina2115@users.noreply.github.com>
2021-02-07 22:50:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+var mymap = L.map('mapid').setView([38.8, -89.58], 8);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
-        });
-      });
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+var marker1 = L.marker([38.56694, -90.49470]).addTo(mymap);
+
+var marker2 = L.marker([38.62521, -90.19111]).addTo(mymap);
+
+var marker3 = L.marker([38.60055, -90.24290]).addTo(mymap);
+
+var marker4 = L.marker([38.62017, -90.27325]).addTo(mymap);
+
+var marker5 = L.marker([38.71714, -90.44806]).addTo(mymap);
+
+marker1.bindPopup("<b>Resturant 1</b><br>Troy Mediterranean Cuisine").openPopup();
+marker2.bindPopup("<b>Resturant 2</b><br>Three Sixty").openPopup();
+marker3.bindPopup("<b>Resturant 3</b><br>The Vine Cafe").openPopup();
+marker4.bindPopup("<b>Resturant 4</b><br>Charlie Gitto's on the Hill").openPopup();
+marker5.bindPopup("<b>Resturant 5</b><br>India Palace").openPopup();
